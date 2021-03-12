a03a6b92c5b38bb559a03ac716d2a6dda9d3257e
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2021-03-12 10:25:35 -0600

    CRUD tasks server side working fine

--- taskorgapi/views/task.py
+++ taskorgapi/views/task.py
@@ -1,47 +1,130 @@
-"""Category ViewSet and Serializers"""
+"""View module for handling requests about tasks"""
 from django.core.exceptions import ValidationError
+from rest_framework import status
+from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import status, serializers
-from taskorgapi.models import Tasks, Tags, Categories, TaskTags
+from rest_framework import serializers
 from django.contrib.auth.models import User
+from taskorgapi.models import Tags, Tasks, Categories, TaskTags
+from rest_framework.decorators import action
 
 
 class TasksView(ViewSet):
-    """Todo view set"""
 
-    def list(self,request):
-        """GET a Todo object"""
-        app_user = User.objects.get(id=request.auth.user.id)
 
-        tasks = Tasks.objects.filter(user_id=app_user)
+    def list(self, request):
+        #Handles get all posts from the database
+
+        tasks = Tasks.objects.filter(user= request.auth.user)
+      
+        serializer = TasksSerializer(
+            tasks, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def create(self, request):
         
-        # e.g.: /todos?categories=1
-        category_id = self.request.query_params.get('categories', None)
-        if category_id is not None:
-            tasks = tasks.filter(category_id=category_id)
+        task_user = User.objects.get(pk=request.auth.user.id)
 
-        # e.g.: /todos?tags=1
-    #first find the tag id the client wants to filter by
-    #if tag is there then we need to find all todos that contain that tag in its tags array
-        tag_id = self.request.query_params.get('tagId', None)
-        if tag_id is not None:
-            tasktags = TaskTags.objects.filter(tag_id=tag_id)
-            tasks = [tasktag.task for tasktag in tasktags]
-                    
+        task = Tasks()
+        task.title = request.data["title"]
+        task.description = request.data["description"]
+        task.create_date_time = request.data["create_date_time"]
+        task.due_date_time = request.data["due_date_time"]
+        task.user = task_user
 
-        serialized_todos = TaskSerializer(tasks, many=True, context={'request': request})
-        return Response(serialized_tasks.data, status=status.HTTP_200_OK)
+        category = Categories.objects.get(pk=request.data["category"])
+
+        task.category = category
+
+        try:
+            task.save()
+            serializer = TasksSerializer(task, context={'request': request})
+            return Response(serializer.data)
+        
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single task
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            task = Tasks.objects.get(pk=pk)
+            task.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Post.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single task
 
-# class TaskSerializer(serializers.ModelSerializer):
-#     """JSON serializer for Task"""
-#     user = UserSerializer(many=False)
-#     category = CategoriesSerializer(many=False)
-#     tags = TaskTagsSerializer(many=True)
+        Returns:
+            Response -- JSON serialized post instance
+        """
 
-#     class Meta:
-#         model = Tasks
-#         fields = ('id', 'user', 'category', 'create_date_time', 'title', 'description', 'due_date_time')
-#         depth = 1
+        try: 
+            # Finds the post by the id provided by URL
+            task = Tasks.objects.get(pk=pk)
+            # Filters tags matching the post primary key via the Tag_Post
+            # matching_tags = Tag.objects.filter(tags__post=post)
+            # Joins the tags to the post object
+            # print(matching_tags.query)
+            # post.tags=matching_tags
+
+
+            serializer = TasksSerializer(task, context={'request': request}) 
+            return Response(serializer.data)
+
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+    
+    def update(self, request, pk=None):
+        # user = Rareuser.objects.get(user=request.auth.user)
+        task_user = User.objects.get(pk=request.auth.user.id)
+
+        task = Tasks.objects.get(pk=pk)
+        task.title = request.data["title"]
+        task.description = request.data["description"]
+        task.create_date_time = request.data["create_date_time"]
+        task.due_date_time = request.data["due_date_time"]
+
+        task.user = task_user
+
+        category = Categories.objects.get(pk=request.data['category'])
+        task.category = category
+        
+        task.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tags
+        fields = ('id', 'label')
+
+class TasksSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tasks
+        fields = ( 'id', 'user', 'category', 'create_date_time', 'title', 'description', 'due_date_time' )
+
+
+class Task_w_TagSerializer(serializers.ModelSerializer):
+    """ Serializer to Join Post and Tags """
+    # Defines the 'tags' field in the Serializer
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Tasks
+        fields = ('id', 'user', 'category', 'create_date_time', 'title', 'description', 'due_date_time', 'tags')
+        depth = 2
